71131acf717f632f1ea7fbab136beccec900aa21
Eusebiu Larion <sebilarion@gmail.com>
2020-12-29 16:41:23 +0200

    adding files to project

---
+++ dist/script.js
@@ -0,0 +1,11 @@
+
+function toogle() {
+  var button= document.getElementById("btn-div");
+  var navigationBar=document.getElementById("navbar");
+ 
+    button.classList.toggle('bg-gray-300');
+    navigationBar.classList.toggle('bg-gray-300');
+    navigationBar.classList.toggle('hidden');
+
+  
+}
